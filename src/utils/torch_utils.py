70659bff2a4806a2b3dec282eedb614a4ff520e0
adianliusie <adianliusie98@gmail.com>
2022-07-07 18:13:27 +0100

    added all model sizes

--- src/utils/torch_utils.py
+++ src/utils/torch_utils.py
@@ -7,23 +7,28 @@ from transformers import GPT2LMHeadModel, GPTNeoForCausalLM, OpenAIGPTLMHeadMode
 
 def load_tokenizer(system:str)->'Tokenizer':
     """ downloads and returns the relevant pretrained tokenizer from huggingface """
-    if system == 'dialo_gpt_small' : tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
-    elif system == 'dialo_gpt'     : tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-    elif system == 'gpt'           : tokenizer = OpenAIGPTTokenizerFast.from_pretrained("openai-gpt")
-    elif system == 'gpt2'          : tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
-    elif system == 'gpt_neo'       : tokenizer = GPT2TokenizerFast.from_pretrained("EleutherAI/gpt-neo-1.3B")
-        
+    if   system == 'dialo_gpt_small' : tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
+    elif system == 'dialo_gpt_med'   : tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+    elif system == 'dialo_gpt_large' : tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
+    elif system == 'gpt2_small'      : tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+    elif system == 'gpt2_med'        : tokenizer = GPT2TokenizerFast.from_pretrained("gpt2-medium")
+    elif system == 'gpt2_large'      : tokenizer = GPT2TokenizerFast.from_pretrained("gpt2-large")
+    elif system == 'gpt_small'       : tokenizer = OpenAIGPTTokenizerFast.from_pretrained("openai-gpt")
+    elif system == 'gpt_neo'         : tokenizer = GPT2TokenizerFast.from_pretrained("EleutherAI/gpt-neo-1.3B")
     else: raise ValueError("invalid transfomer system provided")
     return tokenizer
 model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 
 def load_lm_transformer(system:str)->'Model':
     """ downloads and returns the relevant pretrained transformer from huggingface """
-    if system == 'dialo_gpt_small' : trans_model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")
-    elif system == 'dialo_gpt'     : trans_model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
-    elif system == 'gpt'           : trans_model = OpenAIGPTLMHeadModel.from_pretrained("openai-gpt")
-    elif system == 'gpt2'          : trans_model = GPT2LMHeadModel.from_pretrained("gpt2")
-    elif system == 'gpt_neo'       : trans_model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
+    if   system == 'dialo_gpt_small' : trans_model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")
+    elif system == 'dialo_gpt_med'   : trans_model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+    elif system == 'dialo_gpt_large' : trans_model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large")
+    elif system == 'gpt2_small'      : trans_model = GPT2LMHeadModel.from_pretrained("gpt2")
+    elif system == 'gpt2_med'        : trans_model = GPT2LMHeadModel.from_pretrained("gpt2-medium")
+    elif system == 'gpt2_large'      : trans_model = GPT2LMHeadModel.from_pretrained("gpt2-large")
+    elif system == 'gpt_small'       : trans_model = OpenAIGPTLMHeadModel.from_pretrained("openai-gpt")
+    elif system == 'gpt_neo'         : trans_model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
     else: raise ValueError("invalid transfomer system provided")
     return trans_model
 
